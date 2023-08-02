bedb15839e39003e5b9df430910d9e976e6dc122
Luciri Nazario <luciri_nazario1@baylor.edu>
2023-08-02 13:17:16 -0500

    initial commit

--- zipping.py
+++ zipping.py
@@ -2,7 +2,7 @@ list1 = ['a','b','c']
 list2 = [1,2,3]
 list3 = [1.5, 3.1, 5.7]
 
-for item in zip(list1,list2,list3):
+for item in zip(list1,list2,list3): #zip function helps you iterate through many lists at the same time
     l1,l2,l3 = item
     print(l1)
     print(l2)
